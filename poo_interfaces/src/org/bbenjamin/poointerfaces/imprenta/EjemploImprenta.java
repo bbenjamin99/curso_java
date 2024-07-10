2f0e9d59e0728d3a535ffb0f41c8fdcefaf741bb
bbenjamin99 <bbenjamin4253@gmail.com>
2024-07-10 16:23:22 -0300

    mejorando el codigo de interfaces...

--- poo_interfaces/src/org/bbenjamin/poointerfaces/imprenta/EjemploImprenta.java
+++ poo_interfaces/src/org/bbenjamin/poointerfaces/imprenta/EjemploImprenta.java
@@ -5,16 +5,16 @@ import org.bbenjamin.poointerfaces.imprenta.modelo.*;
 public class EjemploImprenta{
     public static void main(String[] args ) {
         Curriculum cv1 = new Curriculum("Resúmen laboral...", "Benjamin Benitez", "Software Enginer");
-        cv1.addExperiencia("JAVA Developer JR");
-        cv1.addExperiencia("Sring Framework");
-        cv1.addExperiencia("Soporte técnico");
+        cv1.addExperiencia("JAVA Developer JR")
+        .addExperiencia("Sring Framework")
+        .addExperiencia("Soporte técnico");
 
         Informe informe1 = new Informe("Estudio sobre Micro servicios", "Benjamín Benitez","James Fowler ");
         Libro libro1 = new Libro("Benitez", "Road to Software Developer", Genero.ACCION);
 
 
-        libro1.addPagina(new Pagina("Pagina 1..."));
-        libro1.addPagina(new Pagina("Pagina 2..."));
+        libro1.addPagina(new Pagina("Pagina 1..."))
+        .addPagina(new Pagina("Pagina 2..."));
 
     
 
--- poo_interfaces/src/org/bbenjamin/poointerfaces/imprenta/modelo/Imprimible.java
+++ poo_interfaces/src/org/bbenjamin/poointerfaces/imprenta/modelo/Imprimible.java
@@ -1,5 +1,5 @@
 package org.bbenjamin.poointerfaces.imprenta.modelo;
 
 public interface Imprimible {
-    public String imprimir();
+     String imprimir();
 }
