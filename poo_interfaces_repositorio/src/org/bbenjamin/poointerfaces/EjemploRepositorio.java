0cd7b27260c2037d65fc43b78f3ccbbb5cd419bd
bbenjamin99 <bbenjamin4253@gmail.com>
2024-07-12 15:34:11 -0300

    Creando la clase main y probando el CRUD

---
+++ poo_interfaces_repositorio/src/org/bbenjamin/poointerfaces/EjemploRepositorio.java
@@ -0,0 +1,46 @@
+package org.bbenjamin.poointerfaces;
+
+import java.util.List;
+import org.bbenjamin.poointerfaces.modelo.Cliente;
+import org.bbenjamin.poointerfaces.repositorio.ClienteListRepositorio;
+import org.bbenjamin.poointerfaces.repositorio.CrudRepositorio;
+import org.bbenjamin.poointerfaces.repositorio.Direccion;
+import org.bbenjamin.poointerfaces.repositorio.OrdenableRepositorio;
+import org.bbenjamin.poointerfaces.repositorio.PaginableRepositorio;
+
+public class EjemploRepositorio {
+    public static void main(String[] args) {
+
+        CrudRepositorio repo = new ClienteListRepositorio();
+        repo.crear( new Cliente("Benjamin", "Benitez"));
+        repo.crear( new Cliente("Ricardo", "Ioirio"));
+        repo.crear(new Cliente("Lionel", "Messi"));
+        repo.crear(new Cliente("Andres", "Guzman"));
+
+        List<Cliente> clientes = repo.listar();
+        clientes.forEach(System.out::println);
+        System.out.println("===== Paginable =====");
+        List<Cliente> paginar = ((PaginableRepositorio)repo).listar(2,4);  
+        paginar.forEach(System.out::println);
+        System.out.println("\n");
+        System.out.println("===== Ordenable =====");
+        List<Cliente> ordenar = ((OrdenableRepositorio)repo).listar("id", Direccion.DESC);
+        ordenar.forEach(System.out::println);
+        System.out.println("\n");
+
+        System.out.println("===== Editar =====");
+        Cliente editarSegundo = new Cliente("Kylian", "Mbappe");
+        editarSegundo.setId(2);
+        repo.editar(editarSegundo);
+        System.out.println("\n");
+
+        System.out.println("Una vez editado el segundo:");
+        clientes.forEach(System.out::println);
+        System.out.println("\n");
+
+        System.out.println("===== Eliminar ====");
+        repo.eliminar(2);
+        
+        clientes.forEach(System.out::println);
+    }   
+}
